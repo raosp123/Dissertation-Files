ddc4b744463860eaa0c1bc85d30026f3d449464c
pascal.raos <pascal.raos@coinmetrics.io>
2024-01-08 22:35:52 +0000

    cleanup script

--- build-image.sh
+++ build-image.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-kubectl delete -f yaml/http3-app.yaml
+# set -euo pipefail
 
-minikube image rm http3-tools
+# kubectl delete -f yamls/http3-app.yaml
 
-minikube image build -t http3-tools .
+# minikube image rm http3-tools
 
-kubectl apply -f yaml/http3-app.yaml
+# minikube image build -t http3-tools .
 
-docker image ls | grep "<none>" | awk '{print $3 }' | xargs docker image rm # delete unused images
+# kubectl apply -f yaml/http3-app.yaml
 
+
+OLD_CONTAINERS=$(docker image ls | grep "<none>")
+
+NUM_CONTAINER=$(OLD_CONTAINERS | wc -l)
+
+if [[ -n "${OLD_CONTAINERS}" ]]; then
+
+        read -p "Do you want to clean up ${NUM_CONTAINER} unused images y/n? " -n 1 -r
+        if [[ ! $REPLY =~ ^[Yy]$ ]] ; then
+            exit 1
+        fi
+        
+        echo -e "deleting $( ${OLD_CONTAINERS} | awk '{print $1}' ) \n\n"
+        echo "${OLD_CONTAINERS}" | awk '{print $3}'| xargs docker image rm  # delete unused images
+    else
+        echo "No <none> type images to clean up"
+fi
