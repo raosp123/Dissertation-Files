868617ef977a4ce96a0e1f4c03424983c8b90972
Pascal <raosp@tcd.ie>
2024-04-17 18:41:46 +0100

    cleaner setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -28,17 +28,15 @@ if [[ "$HOST_ID" != "0" ]]; then
 
         if [[ "$HOST_ID" = "server" ]]; then
             echo "launching http3-tools in $HOST_ID mode"
-            /usr/bin/screen -LdmS http3-server python3 /opt/aioquic/examples/http3_server.py --certificate /opt/aioquic/tests/ssl_cert.pem --private-key /opt/aioquic/tests/ssl_key.pem --secrets-log /opt/ssl-keylog-secrets.log
+            /usr/bin/screen -LdmS python3 /opt/aioquic/examples/http3_client.py --ca-certs /opt/aioquic/tests/pycacert.pem https://http3-server:4433/ --secrets-log /opt/ssl-keylog-secrets.log -k
+			while true ; do
+			sleep 600
+			echo -e "Currently running server at $(date) is: \n\n$(ps -ef | grep "[p]ython" | awk '{print $8, $9}')"
+		done
         elif [[ "$HOST_ID" = "client" ]]; then
             echo "launching http3-tools in $HOST_ID mode"
-            /usr/bin/screen -LdmS http3-client python3 /opt/aioquic/examples/http3_client.py --ca-certs /opt/aioquic/tests/pycacert.pem https://localhost:4433/
+            /usr/bin/screen -LdmS python3 /opt/aioquic/examples/http3_client.py --ca-certs /opt/aioquic/tests/pycacert.pem https://http3-server:4433/ --secrets-log /opt/ssl-keylog-secrets.log -k
         fi
-
-		while true ; do
-			sleep 600
-			echo -e "Currently running servers at $(date) are: \n\n$(ps -ef | grep "[p]ython" | awk '{print $8, $9}')"
-		done
-
     else
         echo "No host type specified, use flags --server or --client"
 fi
